556193236fe1b535aaf0b395bfce152d501c4ed3
saranyasankar976-lab <saranyasankar976@gmail.com>
2026-09-25 19:21:30 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,34 @@
-import json, os
+import json
+import os
+
 FILE = "data.json"
+
 def save_user_data(name, age, gender, height, weight, goal, activity, bmi, plan):
     data = []
     if os.path.exists(FILE):
         try:
             with open(FILE, "r") as f:
                 data = json.load(f)
-        except: data = []
-    data.append({"name": name, "age": age, "bmi": bmi, "goal": goal})
+        except:
+            data = []
+    data.append({
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "height": height,
+        "weight": weight,
+        "goal": goal,
+        "activity": activity,
+        "bmi": bmi
+    })
     with open(FILE, "w") as f:
         json.dump(data, f, indent=2)
+
+def get_all_users():
+    if os.path.exists(FILE):
+        try:
+            with open(FILE, "r") as f:
+                return json.load(f)
+        except:
+            return []
+    return []
